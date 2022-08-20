50ae3f448932d0c39e3712fd0d4d61b9f4d49663
Amritesh21 <amritesh999singh@gmail.com>
2022-08-20 11:28:32 +0530

    server side changes setup

---
+++ wanderersfamily/src/main/java/apis/UpTestClass.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apis;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author amrit
+ */
+@Path("/test")
+public class UpTestClass {
+    
+    @Path("/call1")
+    @GET
+    @Produces("application/json")
+    public List<String> testMethod(){
+        ArrayList<String> name = new ArrayList();
+        name.add("amritesh");
+        name.add("singh");
+        return name;
+    }
+    
+}
